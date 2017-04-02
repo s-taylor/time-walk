60fde32c101c69022dfc8b6471b4099c09b96b9d
Simon Taylor <nizmox@gmail.com>
2017-04-03 08:29:07 +1000

    eslint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const moment = require('moment-timezone');
 
 class SimpleRecur {
   constructor(start, interval, timezone = 'UTC') {
-t    this.start = moment(start); // why UTC invalid?
+    this.start = moment(start); // why UTC invalid?
     this.interval = interval;
     this.timezone = timezone;
     // TODO validate start format and interval and timezone
@@ -12,20 +12,20 @@ t    this.start = moment(start); // why UTC invalid?
   firstOccurances(count) {
     let result = [this.start.clone()];
 
-    _.times(count - 1, (i) => {
+    _.times((count - 1), (i) => {
       const last = result[i];
       const next = last.clone().add(this.interval);
-      result = [...result, next]
-    })
+      result = [...result, next];
+    });
 
     return result;
   }
 }
 
 const start = [2000, 0, 1];
-const interval = { months: 1 }
+const interval = { months: 1 };
 const count = 12;
 
 const recurTest = new SimpleRecur(start, interval);
-const first = recurTest.firstOccurances(12);
+const first = recurTest.firstOccurances(count);
 console.log('first', first);
