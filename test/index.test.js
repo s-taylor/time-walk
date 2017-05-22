b94ab822d1bd4cc4054499e1007115dbb8a6d2a1
Simon Taylor <nizmox@gmail.com>
2017-05-22 15:41:20 +1000

    test for using singular intervals

--- test/index.test.js
+++ test/index.test.js
@@ -196,14 +196,21 @@ test('.between - daylight savings test', (t) => {
 
 // .getAvgInterval tests
 
-test('.getAvgInterval - calculates correct values using words', (t) => {
-  const interval = { months: 2, days: 1 };
+test('.getAvgInterval - calculates correct values using plural words', (t) => {
+  const interval = { months: 2, days: 3 };
   const result = getAvgInterval(interval);
-  const expected = (values.Month * 2) + values.Day;
+  const expected = (values.Month * 2) + (values.Day * 3);
 
   t.is(result, expected);
 });
 
+test('.getAvgInterval - calculates correct values using singular words', (t) => {
+  const interval = { day: 1, minute: 1 };
+  const result = getAvgInterval(interval);
+  const expected = values.Day + values.Minute;
+
+  t.is(result, expected);
+});
 
 test('.getAvgInterval - calculates correct values using shorthand', (t) => {
   const interval = { Q: 2, m: 30 };
